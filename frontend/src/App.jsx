c9a31c98fb772c21a59751aed89a94702fad27a3
hydrazin60 <sureshkhdk45@gmail.com>
2024-06-18 22:42:48 -0700

    About , dashbord , Home , Project ,signin ,signup page create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
- import React from 'react'
- 
- export default function App() {
-   return (
-     <div>
- hello
- <p>jiban</p>
-     </div>
-   )
-  }
+import React from "react";
+import { BrowserRouter , Routes , Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Project from "./pages/Project"
+import Dashboard from "./pages/Dashboard"
+import Signin from "./pages/Signin"
+import SignUp from "./pages/SignUp"
+export default function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/project" element={<Project/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
