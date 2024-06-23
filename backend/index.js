0b9351687a582bb5c08ba4efabd5930edc8b11e8
hydrazin60 <sureshkhdk45@gmail.com>
2024-06-23 03:53:45 -0700

    mongoDb User Schema create

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const PORT =  process.env.PORT ;
+const PORT = process.env.PORT;
 
-mongoose.connect(process.env.MONGODBURL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGODBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("MongoDB is connected");
   })
