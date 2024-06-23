33a242e6f72a8eacebc064a45e50f4df64fbdaef
hydrazin60 <sureshkhdk45@gmail.com>
2024-06-23 04:39:32 -0700

    only test api create

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODBURL, {
@@ -18,6 +23,10 @@ mongoose
   .catch((error) => {
     console.log("MongoDB connection error:", error);
   });
+
+// Mounting the routers
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
