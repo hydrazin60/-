3dad3cf1e97b95201abb1ffdbaa5d970c68a8a7a
hydrazin60 <sureshkhdk45@gmail.com>
2024-06-23 02:42:59 -0700

    header complede

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaMoon, FaSun, FaSearch } from "react-icons/fa";
 import { HiMenu } from "react-icons/hi";
 
 export default function Header() {
-  const handleSubmit = () => {
-    setIsSignedIn(true);
-  };
-
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
   const navigate = useNavigate();
 
+  const handleSearchClick = () => {
+    setIsSearchVisible(!isSearchVisible);
+  };
+
   return (
-    <div className=" ">
+    <div className="">
       <Navbar className="border-b-2" style={{ backgroundColor: "black" }}>
         <Navbar.Brand>
           <img
@@ -25,7 +26,8 @@ export default function Header() {
             <Button
               onClick={() => navigate("/signin")}
               color={"gray"}
-              className="bg-zinc-400 text-green-500  "
+              gradientDuoTone={"purpleToBlue"}
+              outline
             >
               Sign in{" "}
             </Button>
@@ -33,7 +35,9 @@ export default function Header() {
         </Navbar.Brand>
 
         <div className="flex md:order-2 items-center">
-          <form className="flex">
+          <form
+            className={`flex ${isSearchVisible ? "block" : "hidden"} md:flex`}
+          >
             <input
               type="text"
               placeholder="search..."
@@ -41,29 +45,42 @@ export default function Header() {
               style={{ borderRadius: "50px 0 0 50px" }}
             />
             <span
-              className="rounded-r-3xl border-black border-2  bg-white flex justify-center items-center p-2  "
+              className="rounded-r-3xl border-black border-2 bg-white flex justify-center items-center p-2"
               style={{ borderRadius: "0 50px 50px 0" }}
             >
               <FaSearch className="bg-white text-black" />
             </span>
           </form>
-
-          <span className="ml-4">
-            <Navbar.Toggle />
-          </span>
+          {!isSearchVisible && (
+            <button
+              style={{ marginRight: "10px" }}
+              className="text-black h-10 w-10 bg-white rounded-full flex items-center justify-center md:hidden"
+              onClick={handleSearchClick}
+            >
+              <FaSearch className="text-xl" />
+            </button>
+          )}
+          <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
           <Button
             onClick={() => navigate("/about")}
             color={"gray"}
-            className="bg-zinc-400 text-green-500  "
+            gradientDuoTone={"purpleToBlue"}
+            style={{ margin: "5px" }}
+            className="w-20"
+            outline
           >
             About
           </Button>
+
           <Button
             onClick={() => navigate("/project")}
             color={"gray"}
-            className="bg-zinc-400 text-green-500  "
+            gradientDuoTone={"purpleToBlue"}
+            style={{ margin: "5px" }}
+            className="w-20"
+            outline
           >
             Project
           </Button>
